4e2a96d38d970e8f5f1e14ea24c2836da5ac5ac8
Prajyot-Ghadi <prajyotghadi89@gmail.com>
2026-08-08 23:42:50 +0530

    Implement Update Operation

--- employees/urls.py
+++ employees/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:id>/', views.employee_detail, name='employee_detail'),
-    path('add/', views.add_employee, name='add_employee')
+    path('add/', views.add_employee, name='add_employee'),
+    path('update/<int:id>/', views.update_employee, name='update_employee'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -31,8 +31,33 @@ def add_employee(request):
         else:
             print(form.errors)
 
-    form = EmployeeForm()
+    else:
+        form = EmployeeForm()
     context = {
         'form' : form,
     }
     return render(request, 'add_employee.html', context)
+
+
+
+def update_employee(request, id):
+
+    employee = get_object_or_404(Employee, id = id)
+
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES, instance=employee)
+
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.errors)
+
+    else:
+        form = EmployeeForm(instance=employee)
+        
+    context = {
+                'form':form,
+                'employee':employee
+                
+            }
+    return render(request, 'update_employee.html', context)
